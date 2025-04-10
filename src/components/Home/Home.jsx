51e6bde2785f06290cc0e5c3aad473ca5aefec6f
Maksudul <maaksuduldish@gmail.com>
2025-04-10 12:14:36 +0600

    File initialized

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to Home page of Firebase</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/layouts/Root.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header/Header";
+import Home from "../components/Home/Home";
+
+const Root = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Home></Home>
+        </div>
+    );
+};
+
+export default Root;
