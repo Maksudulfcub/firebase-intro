edd5cf021f7df79506623c6e382d7d6fdde77f4f
Maksudul <maaksuduldish@gmail.com>
2025-04-14 11:45:55 +0600

    Context created

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,15 @@
+import { createContext } from "react";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+    const authInfo = { name: 'Context API' }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import Root from './layouts/Root.jsx';
 import Home from './components/Home/Home.jsx';
 import Login from './components/Login/Login.jsx';
 import Register from './components/Register/Register.jsx';
+import AuthProvider from './context/AuthProvider.jsx';
 
 const router = createBrowserRouter([
   {
@@ -34,6 +35,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
