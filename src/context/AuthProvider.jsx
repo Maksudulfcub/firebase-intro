a05ab1d93f708afc52fa162d42b202e98e068097
Maksudul <maaksuduldish@gmail.com>
2025-04-15 16:36:37 +0600

    loading state implemented

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -7,16 +7,20 @@ export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     const createUser = (email, password) => {
+        setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
     const loginUser = (email, password) => {
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     }
 
     const logOut = () => {
+        setLoading(true)
         return signOut(auth);
     }
 
@@ -24,13 +28,14 @@ const AuthProvider = ({ children }) => {
         const unSubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
             console.log('current user info is', currentUser);
+            setLoading(false)
         });
         return () => {
             unSubscribe;
         }
     }, [])
 
-    const authInfo = { user, createUser, loginUser, logOut }
+    const authInfo = { user, loading, createUser, loginUser, logOut }
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
